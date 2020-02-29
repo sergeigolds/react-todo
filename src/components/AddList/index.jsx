78d7eff0874d9a21ef0538cc34a7d3eac2616125
Sergei Golds <sergei.golds@gmail.com>
2020-02-29 22:28:36 +0200

    added fake json server

--- src/components/AddList/index.jsx
+++ src/components/AddList/index.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import List from "../List";
 import Badge from "../Badge";
 
@@ -8,8 +10,15 @@ import "./AddList.scss";
 
 const AddList = ({ colors, onAdd }) => {
   const [isVisiblePopup, setVisiblePopup] = useState(false);
-  const [selectedColor, selectColor] = useState(colors[0].id);
+  const [selectedColor, selectColor] = useState(3);
+  const [isLoading, setIsLoading] = useState(false);
   const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    if (Array.isArray(colors)) {
+      selectColor(colors[0].id);
+    }
+  }, [colors]);
 
   const onClosePopup = () => {
     setVisiblePopup(false);
@@ -23,9 +32,21 @@ const AddList = ({ colors, onAdd }) => {
       return;
     }
 
-    const color = colors.filter(c => c.id === selectedColor)[0].name;
-    onAdd({ id: Math.random(), name: inputValue, color });
-    onClosePopup();
+    setIsLoading(true);
+    axios
+      .post("http://localhost:3001/lists", {
+        name: inputValue,
+        colorId: selectedColor
+      })
+      .then(({ data }) => {
+        const color = colors.filter(c => c.id === selectedColor)[0].name;
+        const listObj = { ...data, color: { name: color } };
+        onAdd(listObj);
+        onClosePopup();
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
@@ -92,7 +113,7 @@ const AddList = ({ colors, onAdd }) => {
           </div>
 
           <button onClick={addNewCategory} className="button">
-            Add category
+            {isLoading ? "Add in progress..." : "Add new category"}
           </button>
         </div>
       )}
